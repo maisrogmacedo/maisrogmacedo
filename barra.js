dce8c8306770295cb596407f076411c461a3b24e
maisrogmacedo <rogmacedo1793@gmail.com>
2025-09-20 16:42:38 -0300

    Update barra.js

--- barra.js
+++ barra.js
@@ -5,7 +5,7 @@
   const container = document.createElement("div");
   container.id = "barra-maisrog";
   container.innerHTML = `
-    <a href="https://www.maisrogmacedo.wordpress.com/" class="mr-link">+Rogmacedo</a>
+    <a href="https://www.maisrogmacedo.wordpress.com" class="mr-link">+Rogmacedo</a>
   `;
 
   const style = document.createElement("style");
